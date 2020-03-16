f02a810b29a98e6f5a0fb15c017d057cd82fc742
Andres <bicho_suarez@hotmail.com>
2020-03-16 02:40:03 -0700

    Gameplay changes & next piece window function added

--- tetris2.js
+++ tetris2.js
@@ -3,6 +3,7 @@ const ctx = cvs.getContext("2d");
 const scoreElement = document.getElementById("score");
 const levelElement = document.getElementById("level");
 const nextPieceElement = document.getElementById("nextPiece");
+const contElementosElement = document.getElementById("contElementos");
 
 //TODO: Crear objeto json de elemento  (nombre y simbolo)
 //TODO: Crear arreglo de objetos elementos
@@ -10,10 +11,16 @@ const nextPieceElement = document.getElementById("nextPiece");
 //TODO: Crear tarjetas de elementos
 //TODO: validar mostrar tarjetas según puntaje
 
+let currentLVL = 0;
+let nxtLvl = currentLVL+1000;
+
 const ROW = 17;
 const COL = (COLUMN = 10);
 const SQ = (squareSize = 25);
 const VACANT = "WHITE"; // color of an empty square
+
+const LVL0 = 0, LVL1 = 999, LVL2 = 1999, LVL3 = 2999, LVL4 = 3999, LVL5 = 4999, LVL6 = 5999,
+      LVL7 = 6999, LVL8 = 7999, LVL9 = 8999, LVL10 = 9999, LVL11 = 1999, LVL12 = 11999, LVL13 = 12999;
 
 // draw a square
 function drawSquare(x, y, color) {
@@ -115,6 +122,8 @@ var cont = 0;
 let nxtPieceToDraw = arregloPiezas[1];
 console.log(arregloPiezas);
 
+checkNextPiece();
+
 console.log(nxtPieceToDraw);
 
 Piece.prototype.moveDown = function() {
@@ -125,19 +134,41 @@ Piece.prototype.moveDown = function() {
   } else {
     // we lock the piece and generate a new one
     this.lock();
-    //p = randomPiece();
+    p = randomPiece();
     cont++;
     let nextPieceCont = cont+1;
     nxtPieceToDraw = arregloPiezas[nextPieceCont];
     p = arregloPiezas[cont];
-    console.log(nxtPieceToDraw);
+    //console.log(nxtPieceToDraw);
+    //console.log(nxtPieceToDraw.color);
+    checkNextPiece();
+
   }
 };
 
-
-
-function drawNextPiece(){
-  nxtPiesceToDraw.fill();
+function checkNextPiece(){
+  if(nxtPieceToDraw.color == '#FF0D72'){
+    console.log("Sigue Z");
+    nextPieceElement.innerHTML = "<img src=\"./images/z.png\">";
+  } else if (nxtPieceToDraw.color == '#0DC2FF'){
+    console.log("Sigue S");
+    nextPieceElement.innerHTML = "<img src=\"./images/s.png\">";
+  } else if (nxtPieceToDraw.color == '#0DFF72'){
+    console.log("Sigue T");
+    nextPieceElement.innerHTML = "<img src=\"./images/t.png\">";
+  } else if (nxtPieceToDraw.color == '#F538FF'){
+    console.log("Sigue O");
+    nextPieceElement.innerHTML = "<img src=\"./images/o.png\">";
+  } else if (nxtPieceToDraw.color == '#FF8E0D'){
+    console.log("Sigue L");
+    nextPieceElement.innerHTML = "<img src=\"./images/l.png\">";
+  } else if (nxtPieceToDraw.color == '#FFE138'){
+    console.log("Sigue I");
+    nextPieceElement.innerHTML = "<img src=\"./images/i.png\">";
+  } else if (nxtPieceToDraw.color == '#3877FF'){
+    console.log("Sigue J");
+    nextPieceElement.innerHTML = "<img src=\"./images/j.png\">";
+  }
 }
 
 // move Right the piece
@@ -193,8 +224,8 @@ const alerChido = () =>
     confirmButtonText: "¡Terminar!"
   }).then(function() {
     window.location =
-      "file:///Applications/MAMP/htdocs/SimpleTetris/index2.html";
-      //"file:///C:/wamp64/www/SimpleTetris/index2.html";
+      //"file:///Applications/MAMP/htdocs/SimpleTetris/index2.html";
+      "file:///C:/wamp64/www/SimpleTetris/index2.html";
   });
 
 Piece.prototype.lock = function() {
@@ -225,6 +256,7 @@ Piece.prototype.lock = function() {
     if (isRowFull) {
       // if the row is full
       // we move down all the rows above it
+
       for (y = r; y > 1; y--) {
         for (c = 0; c < COL; c++) {
           board[y][c] = board[y - 1][c];
@@ -233,9 +265,12 @@ Piece.prototype.lock = function() {
       // the top row board[0][..] has no row above it
       for (c = 0; c < COL; c++) {
         board[0][c] = VACANT;
+        //contR++;
       }
+
       // increment the score
-      score += 10 * 17;
+      score += 250;
+
       scoreElement.innerHTML = score;
     }
   }
@@ -304,80 +339,119 @@ function drop() {
 
   //console.log(delta);
 
-  if (score < 200) {
+  if (score < LVL1) {
     if (delta > 650) {
       p.moveDown();
       dropStart = Date.now();
       // update the score
-      levelElement.innerHTML = 1;
+      levelElement.innerHTML = 0;
+      currentLVL = 0;
+      nxtLvl = LVL1;
     }
   }
-  if (score >= 200 && score < 1000) {
+  if (score >= LVL1 && score < LVL2) {
     if (delta > 550) {
       p.moveDown();
       dropStart = Date.now();
       // update the score
-      levelElement.innerHTML = 2;
+      levelElement.innerHTML = 1;
+      currentLVL = 1;
+      nxtLvl = LVL2;
     }
-  } else if (score >= 1000 && score < 2000) {
+  } else if (score >= LVL2 && score < LVL3) {
     if (delta > 450) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 3;
+      levelElement.innerHTML = 2;
+      currentLVL = 2;
+      nxtLvl = LVL3;
     }
-  } else if (score >= 2000 && score < 3000) {
+  } else if (score >= LVL3 && score < LVL4) {
     if (delta > 425) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 4;
+      levelElement.innerHTML = 3;
+      currentLVL = 3;
+      nxtLvl = LVL4;
     }
-  } else if (score >= 3000 && score < 4000) {
+  } else if (score >= LVL4 && score < LVL5) {
     if (delta > 410) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 5;
+      levelElement.innerHTML = 4;
+      currentLVL = 4;
+      nxtLvl = LVL5;
     }
-  } else if (score >= 4000 && score < 5000) {
+  } else if (score >= LVL5 && score < LVL6) {
     if (delta > 400) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 6;
+      levelElement.innerHTML = 5;
+      currentLVL = 5;
+      nxtLvl = LVL6;
     }
-  } else if (score >= 5000 && score < 6000) {
+  } else if (score >= LVL6 && score < LVL7) {
     if (delta > 390) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 7;
+      levelElement.innerHTML = 6;
+      currentLVL = 6;
+      nxtLvl = LVL7;
     }
-  } else if (score >= 6000 && score < 7000) {
+  } else if (score >= LVL7 && score < LVL8) {
     if (delta > 380) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 8;
+      levelElement.innerHTML = 7;
+      currentLVL = 7;
+      nxtLvl = LVL8;
     }
-  } else if (score >= 7000 && score < 8000) {
+  } else if (score >= LVL8 && score < LVL9) {
     if (delta > 370) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 9;
+      levelElement.innerHTML = 8;
+      currentLVL = 8;
+      nxtLvl = LVL9;
     }
-  } else if (score >= 8000 && score < 9000) {
+  } else if (score >= LVL9 && score < LVL10) {
     if (delta > 360) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 10;
+      levelElement.innerHTML = 9;
+      currentLVL = 9;
+      nxtLvl = LVL10;
     }
-  } else if (score >= 9000 && score < 10000) {
+  } else if (score >= LVL10 && score < LVL11) {
     if (delta > 350) {
       p.moveDown();
       dropStart = Date.now();
-      levelElement.innerHTML = 11;
+      levelElement.innerHTML = 10;
+      currentLVL = 10;
+      nxtLvl = LVL11;
     }
-  } else if (score >= 10000) {
+  } else if (score >= LVL11 && score < LVL12) {
     if (delta > 340) {
       p.moveDown();
       dropStart = Date.now();
+      levelElement.innerHTML = 11;
+      currentLVL = 11;
+      nxtLvl = LVL12;
+    }
+  } else if (score >= LVL12 && score < LVL13) {
+    if (delta > 330) {
+      p.moveDown();
+      dropStart = Date.now();
       levelElement.innerHTML = 12;
+      currentLVL = 12;
+      nxtLvl = LVL13;
+    }
+  } else if (score >= LVL13) {
+    if (delta > 320) {
+      p.moveDown();
+      dropStart = Date.now();
+      levelElement.innerHTML = 13;
+      currentLVL = 13;
     }
   }
 
